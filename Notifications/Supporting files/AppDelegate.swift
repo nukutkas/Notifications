ca1f609f61049c4c0307ec789642ba6b90ca8d71
Nikita Kochetov <nothsmple@mail.ru>
2021-06-05 21:43:26 +0300

    Refactoring, class Notifications has been added.

--- Notifications/Supporting files/AppDelegate.swift
+++ Notifications/Supporting files/AppDelegate.swift
@@ -12,12 +12,12 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    let notificationCenter = UNUserNotificationCenter.current()
+    let notifications = Notifications()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        requestAutorization()
-        notificationCenter.delegate = self
+        notifications.requestAutorization()
+        notifications.notificationCenter.delegate = notifications
         return true
     }
     
@@ -25,86 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIApplication.shared.applicationIconBadgeNumber = 0
     }
     
-    func requestAutorization() {
-        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
-            print("Permission granted: \(granted)")
-            
-            guard granted else { return }
-            self.getNotificationSetttings()
-        }
-    }
-    
-    func getNotificationSetttings() {
-        notificationCenter.getNotificationSettings { (settings) in
-            print("Notification settings: \(settings)")
-        }
-    }
-    
-    func scheduleNotification(notificationType: String) {
-        
-        let content = UNMutableNotificationContent()
-        let userAction = "User Action"
-        
-        
-        content.title = notificationType
-        content.body =  "This is example how to create " + notificationType
-        content.sound = .default
-        content.badge = 1
-        content.categoryIdentifier = userAction
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        
-        let identifire = "Local Notification"
-        let request = UNNotificationRequest(identifier: identifire,
-                                            content: content,
-                                            trigger: trigger)
-        
-        notificationCenter.add(request) { (error) in
-            if let error = error {
-                print("Error \(error.localizedDescription)")
-            }
-        }
-        
-        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
-        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
-        let category = UNNotificationCategory(
-            identifier: userAction,
-            actions: [snoozeAction, deleteAction],
-            intentIdentifiers: [],
-            options: [])
-        
-        notificationCenter.setNotificationCategories([category])
-    }
-    
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-    
-        completionHandler([.alert, .sound])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        if response.notification.request.identifier == "Local Notification" {
-            print("Handling notification with the Local Notification Identifire")
-        }
-        
-        switch response.actionIdentifier {
-        case UNNotificationDismissActionIdentifier:
-            print("Dismiss Action")
-        case UNNotificationDefaultActionIdentifier:
-            print("Default")
-        case "Snooze":
-            print("Snooze")
-            scheduleNotification(notificationType: "Reminder")
-        case "Delete":
-            print("Delete")
-        default:
-            print("Unknown action ")
-        }
-        
-        completionHandler()
-    }
 }
