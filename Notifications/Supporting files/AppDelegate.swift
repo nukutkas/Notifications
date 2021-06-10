e3eda8c875eb155cda79d264f782db36d6d1f7f6
Nikita Kochetov <nothsmple@mail.ru>
2021-06-10 22:33:26 +0300

    FirebaseApp.configure() has been added to AppDelegate,

--- Notifications/Supporting files/AppDelegate.swift
+++ Notifications/Supporting files/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import UserNotifications
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         notifications.requestAutorization()
         notifications.notificationCenter.delegate = notifications
+        
+        FirebaseApp.configure()
+        
         return true
     }
     
